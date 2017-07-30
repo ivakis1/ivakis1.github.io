85c5064bb529f86b8c5f5af970d7f4fef5a50eb5
Ivo Hristov <i_hrisstov@hotmail.com>
2017-07-30 12:32:55 +0300

    Upgraded answers

--- models/Calculator.js
+++ models/Calculator.js
@@ -1,9 +1,13 @@
 function Answer(){
     
-  let answers = ['Yes', 'It\'s certain', 'Very doubtful', 'Without a doubt', 'Signs points to YES',
-      'Definitely YES', 'Outlook not so good', 'Ask again later', 'No', 'Positive', 'As I see it - YES',
-      'Better not tell you now', 'Don\'t count on it', '100%', 'Most likely'];
+  // let answers = ['Yes', 'It\'s certain', 'Very doubtful', 'Without a doubt', 'Signs points to YES',
+  //     'Definitely YES', 'Outlook not so good', 'Ask again later', 'No', 'Positive', 'As I see it - YES',
+  //     'Better not tell you now', 'Don\'t count on it', '100%', 'Most likely'];
 
+    let answers = ['Да', 'Не', 'Определено не', 'По-добре да не ти казвам',
+        'Не искаш да знаеш', 'Със сигурност', 'Определено да', 'Не съм сигурен',
+        'Попитай друг път', 'Не разчитай на това', '100%', 'Не изглежда добре'];
+    
         return answers;
 
 }
