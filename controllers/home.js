71430437455d33dfd70ace68360add47afd5259e
Ivo Hristov <i_hrisstov@hotmail.com>
2017-07-31 20:03:53 +0300

    Insert author link, add required text box for question

--- controllers/home.js
+++ controllers/home.js
@@ -5,14 +5,14 @@ module.exports = {
     indexPost: (req, res) => {
 
         let question = req.body['question'];
-        console.log('wfwef');
+
         let answers = ['Да', 'Не', 'Определено не', 'По-добре да не ти казвам',
             'Не искаш да знаеш', 'Със сигурност', 'Определено да', 'Не съм сигурен',
-            'Попитай друг път', 'Не разчитай на това', '100%', 'Не изглежда добре'];
+            'Попитай друг път', 'Не разчитай на това', '100% да', 'По-скоро не', 'По-скоро да', '100% не' ];
 
         let position = Math.ceil(Math.random() * answers.length);
 
-        if(question.charAt(question.length - 1) !== '?') {
+        if(question.charAt(question.length - 1) !== '?' && question.length > 0) {
             question += "?";
         }
         res.render('home/index', {'question': question, 'answer': answers[position]})
